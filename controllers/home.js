f596e73d72031b4d00da9e7ec5ae0d4ae8476811
Sanam1221 <sehray6789@gmail.com>
2023-01-10 10:35:35 +0530

    initial

---
+++ controllers/home.js
@@ -0,0 +1,8 @@
+
+
+const homeController = (req, res) =>{
+   res.send("server working fine")
+}
+
+
+module.exports = homeController
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const login = require('./routes/login')
 const verify = require('./routes/verify') 
 const { json } = require('express')
 const bodyParser = require("body-parser"); 
+const router = require("./routes/home")
 
 app.use(bodyParser.json());
 app.use(cors())
 app.use(json())
+app.use('', router)
 app.use('/login',login)
 app.use('/verify/:id',verify)
 app.listen(PORT, ()=>{
